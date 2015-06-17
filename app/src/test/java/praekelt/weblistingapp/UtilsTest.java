d6581099dacd779139e7a08327041dd20379f570
AltusBarry <AltusBarry@users.noreply.github.com>
2015-06-17 10:44:19 +0200

    Reupload for travis
    Former-commit-id: 1f6254d5ae14dd817d162dc4b7218aece7297c67

--- app/src/test/java/praekelt/weblistingapp/UtilsTest.java
+++ app/src/test/java/praekelt/weblistingapp/UtilsTest.java
@@ -1,9 +1,34 @@
 package praekelt.weblistingapp;
 
-import junit.framework.TestCase;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+
+import praekelt.weblistingapp.Utils.JSONUtils;
 
 /**
  * Created by altus on 2015/06/11.
  */
-public class UtilsTest extends TestCase {
+public class UtilsTest {
+
+    @Test
+    public void utilsTest() {
+        JsonElement element = new JsonParser().parse(TestConstants.postJSON);
+        try {
+            JSONUtils.getDetailObject(element);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
 }
